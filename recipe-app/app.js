ea232a3257c6b5d5ca5e97b1efb83d794c31376d
MustafoValiev <108987959+MustafoValiev@users.noreply.github.com>
2023-08-08 22:10:42 +0500

    feat: add workable parts of recipe-app

--- recipe-app/app.js
+++ recipe-app/app.js
@@ -1,32 +1,100 @@
 let mealContainer = document.getElementById("meal");
 let container = document.getElementById("list-slide-container")
-let wrapper = document.getElementById("list-slide")
+let wrapper = document.getElementById("list-slide-wrapper")
+let mainContainer = document.getElementById("container")
+let favListElement = document.getElementsByTagName("aside")[0]
 let listImgMeal = []
 let listMeal = []
 let count = 0
+let slideContainerWidth = mainContainer.offsetWidth - 116
+let favArrayID;
+
+// localStorage.clear()
 
 window.onload = () => {
-    for (let i = 0; i <5 ; i++) {
-        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-            .then(res => res.json())
-            .then(res => {
-                createListImgMeal(res.meals[0]);
-                listMeal.push(res.meals[0])
-                let div = document.createElement("div")
-                let img = document.createElement("img")
-                div.classList.add("meal-icon")
-                div.id = `divList${count++}`
-                for (let j = 0; j < count; j++) {
-                    img.setAttribute("src", `${listImgMeal[i]}`)
-                    img.setAttribute("alt", "Meal")
-                    img.classList.add("image")
-                    div.append(img)
-                    container.append(div)
-                }
-            })
-            .catch(e => {
-                console.warn(e);
-            });
+    for (let i = 0; i < slideContainerWidth / 116; i++) {
+        generateMeal(i)
+    }
+    favArrayID = showFavMealsID() ? showFavMealsID().split(",") : []
+    console.log(favArrayID)
+}
+
+function generateMeal(i) {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res => res.json())
+        .then(res => {
+            createListImgMeal(res.meals[0]);
+            listMeal.push(res.meals[0])
+            let div = document.createElement("div")
+            let img = document.createElement("img")
+            let span = document.createElement("span")
+            div.classList.add("meal-icon")
+            div.id = `divList${count++}`
+            span.innerHTML = "❤️"
+            span.id = "fav"
+            for (let j = 0; j < count; j++) {
+                img.setAttribute("src", `${listImgMeal[i]}`)
+                img.setAttribute("alt", "Meal")
+                img.classList.add("image")
+                div.append(span)
+                div.append(img)
+                container.append(div)
+            }
+            // if ()
+        })
+        .catch(e => {
+            console.warn(e);
+        });
+}
+
+window.onclick = e => {
+    if (e.target.id === "fav") {
+        let str = e.target.parentElement.id
+        let favMeal = listMeal[str.charAt(str.length - 1)]
+        if (!favArrayID.includes(favMeal.idMeal)) {
+            favArrayID.push(favMeal.idMeal)
+            saveFavMealsID()
+            let favUl = document.getElementById("fav-ul")
+            let li = document.createElement("LI")
+            li.innerHTML = `
+            <img src="${favMeal.strMealThumb}" alt="Meal Image" id="img-icon">
+            <h3>${favMeal.strMeal}</h3>
+            `
+            favUl.appendChild(li)
+        }
+    }
+    if (e.target.classList.contains("fa-bars")) {
+        e.target.addEventListener('click', function () {
+            let isOpen = favListElement.classList.contains('slide-in');
+            favListElement.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
+            e.target.parentElement.setAttribute('class', isOpen ? 'rotate-out' : 'rotate-in');
+        });
+    }
+    let spanCross = document.getElementById("span-cross");
+
+    if (e.target === spanCross) {
+        mealContainer.innerHTML = ''
+        mealContainer.style.opacity = "0"
+        container.style.filter = "none"
+        container.style.pointerEvents = "initial"
+        container.style.userSelect = "initial"
+    } else if (e.target.classList.contains("image")) {
+        container.style.pointerEvents = "none"
+        container.style.userSelect = "none"
+    }
+}
+
+window.onkeydown = e => {
+    closeEvent(e)
+}
+
+function closeEvent(e) {
+    if (e.key === "Escape") {
+        mealContainer.innerHTML = ''
+        mealContainer.style.opacity = "0"
+        container.style.filter = "none"
+        container.style.pointerEvents = "initial"
+        container.style.userSelect = "initial"
     }
 }
 
@@ -34,16 +102,20 @@ const createListImgMeal = meal => {
     listImgMeal.push(meal.strMealThumb)
 }
 
-console.log(wrapper)
-
 wrapper.addEventListener("click", function (e) {
     let img;
-    if (e.target.classList.contains("image")){
+    if (e.target.classList.contains("image")) {
         img = e.target
         let str = img.parentElement.id
         createMeal(listMeal[str.charAt(str.length - 1)])
+        let youtubeDiv = document.getElementsByClassName("video-div")
+        if (youtubeDiv.length === 0) {
+            let descrp = document.querySelector(".description p")
+            descrp.style.maxHeight = "24rem"
+        }
     }
 })
+
 
 const createMeal = meal => {
     const ingredients = [];
@@ -58,10 +130,11 @@ const createMeal = meal => {
     }
 
     const newInnerHTML = `
+        <span id="span-cross">✕</span>
     	<div class="meal-wrapper">
     		<div class="info-div">
-    			<img src="${meal.strMealThumb}" alt="Meal Image">
-    			<div>
+    			<img src="${meal.strMealThumb}" alt="Meal Image" id="img-icon">
+    			<div id="brief-desc">
     			${
         meal.strCategory
             ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
@@ -86,7 +159,7 @@ const createMeal = meal => {
     		</div>
     		<div class="meal-description">
                 <div class="description">
-                    <h4>${meal.strMeal}</h4>
+                    <h2>${meal.strMeal}</h2>
                     <p>${meal.strInstructions}</p>
                 </div>
                  ${
@@ -94,18 +167,26 @@ const createMeal = meal => {
             ? `
                         <div class="video-div">
                             <h4>Video Recipe</h4>
-                                <div class="videoWrapper" style=" width: 320px">
+                                <div class="videoWrapper" style=" width: 12rem">
                                     <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}"></iframe>
                                 </div>
                         </div>
-                    `
+                        `
             : ''
     }
     		</div>
     	</div>
     	
     `;
-
     mealContainer.innerHTML = newInnerHTML;
-    mealContainer.style.zIndex = "10"
+    mealContainer.style.opacity = "1"
+    container.style.filter = "blur(3px)";
 };
+
+function saveFavMealsID() {
+    localStorage.setItem("favMealsID", favArrayID)
+}
+
+function showFavMealsID() {
+    return localStorage.getItem("favMealsID")
+}
